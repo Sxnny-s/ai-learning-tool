f52a8d5b2f46ad6a7db90afc50125c0ea40a3abc
Sxnny-s <ethsok@protonmail.com>
2025-10-05 20:07:10 -0400

    added session summaries for every student via get API route

--- app/api/analytics/students/route.ts
+++ app/api/analytics/students/route.ts
@@ -1,4 +1,4 @@
-
+// total_sessions, avg_session_length, last_active_at.
 import { createServerSupabaseClient } from '@/lib/supabase/server'
 
 export async function GET() {
@@ -10,5 +10,27 @@ export async function GET() {
     return new Response(JSON.stringify({ error: error.message }), { status: 500 })
   }
 
-  return new Response(JSON.stringify(data), { status: 200 })
+  
+
+  let res = []
+
+
+  data.forEach(student => {
+    let studentObject = {
+      user_id: student['user_id'],
+      name: student['full_name'],
+      total_sessions: student['session_count'],
+      avg_session_length: student['total_time_seconds'] / student['session_count'],
+      last_active_at: student['last_session_ended_at']
+    }
+
+    res.push(studentObject)
+  })
+  
+
+ 
+  return new Response(JSON.stringify({data: res}), { status: 200 })
+  
+
+
 }
